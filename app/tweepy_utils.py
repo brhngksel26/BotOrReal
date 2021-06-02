663eae36a9b1cbfaee4589bcb799f430a1280b70
brhngksel26 <brhngksel@gmail.com>
2021-06-02 11:58:17 +0300

    test

--- app/tweepy_utils.py
+++ app/tweepy_utils.py
@@ -17,6 +17,8 @@ def account_data(username):
     try:
         # Get user information from screen name
         user = api.get_user(username)
+        print("tesr")
+
 
         # account features to return for predicton
         account_age_days = (datetime.now() - user.created_at).days
